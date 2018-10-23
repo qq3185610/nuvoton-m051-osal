71005631a795f180d7ce064f9b54469176d2d2c2
OKMCU <3185610@qq.com>
2018-10-23 08:10:55 +0800

    remove unused source code

--- app/app.c
+++ app/app.c
@@ -19,14 +19,10 @@
  * INCLUDES
  **************************************************************************************************/
 #include "osal.h"
-//#include "hal.h"
 #include "app.h"
 
 #include "aps_cli.h"
-//#include "app_task_key.h"
 #include "app_task_cli.h"
-//#include "app_task_lucicop.h"
-//#include "app_task_bt.h"
 #include "app_task_dongle.h"
 /**************************************************************************************************
  * TYPES
@@ -44,24 +40,9 @@
 
 extern void AppInit ( void )
 {
-    //osal_task_create( AppTask_CLI, OSAL_TASK_ID_APP_CLI );
-    //AppTask_CLI_Init();
-    
-    //osal_task_create( AppTask_Key, OSAL_TASK_ID_APP_KEY );
-    //AppTask_Key_Init();
-
-    //osal_task_create( AppTask_LuciCoP, OSAL_TASK_ID_APP_LUCI_COP );
-    //AppTask_LuciCoP_Init();
-
-    //osal_task_create( AppTask_BT, OSAL_TASK_ID_APP_BT );
-    //AppTask_BT_Init();
-    
     //Application task init and create
     osal_task_create( AppTask_CLI, OSAL_TASK_ID_APP_CLI );
     AppTask_CLI_Init();
-
-    osal_task_create( AppTask_Dongle, OSAL_TASK_ID_APP_DONGLE );
-    AppTask_Dongle_Init();
 
     //Application Support Sublayer module init
     ApsCliInit();
